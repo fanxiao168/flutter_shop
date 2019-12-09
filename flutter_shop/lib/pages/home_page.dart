30398fe7747e8c2c3dd7e3b82da8df2e905b58f4
fanxiao168 <fanxiao168@126.com>
2019-12-09 17:09:26 +0800

    dio基础_引入和简单的Get请求

--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return Scaffold(
       body: Center(child: Text('商城首页')),
     );
+  }
+}
+
+void getHttp() async {
+  try{
+    Response response;
+    response = await Dio().get("https://api.apiopen.top/searchMusic?name=雅俗共赏");
+    return print(response);
+  }catch(e){
+    return print(e);
   }
 }
