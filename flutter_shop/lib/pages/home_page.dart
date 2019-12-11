eb529904ba8efc1c5f88265beda711166d37d2a4
fanxiao168 <fanxiao168@126.com>
2019-12-11 16:43:59 +0800

    Dio基础_POST请求的使用

--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -23,8 +23,8 @@ class _HomePageState extends State<HomePage> {
                     controller: typeController,
                     decoration: InputDecoration(
                       contentPadding: EdgeInsets.all(10.0),
-                      labelText: '美女类型',
-                      helperText: '请输入你喜欢的类型',
+                      labelText: '歌名',
+                      helperText: '请输入歌名',
                     ),
                     autofocus: false,
                   ),
@@ -45,13 +45,13 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _choiceAction() {
-    print('开始选择你喜欢的类型.......');
+    print('开始选择歌名.......');
     if (typeController.text.toString() == '') {
       showDialog(
           context: context,
           builder: (context) => AlertDialog(title: Text('内容不能为空')));
     } else {
-      getHttp(typeController.text.toString()).then((val) {
+      postHttp(typeController.text.toString()).then((val) {
         setState(() {
           print(val);
           showText = val['result'].toString();
@@ -60,12 +60,12 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  Future getHttp(String TypeText) async {
+  Future postHttp(String TypeText) async {
     try {
       Response response;
       var data = {'name': TypeText};
       response = await Dio()
-          .get("https://api.apiopen.top/searchMusic", queryParameters: data);
+          .post("https://api.apiopen.top/searchMusic", queryParameters: data);
       return response.data;
     } catch (e) {
       return print(e);
