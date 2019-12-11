12b0f7232e0c6d3cb03f17cfce434d200bb6052b
fanxiao168 <fanxiao168@126.com>
2019-12-11 15:58:05 +0800

    fix bug

--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -7,7 +7,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  
   TextEditingController typeController = TextEditingController();
   String showText = '欢迎您来到美好人间高级会所';
 
@@ -15,60 +14,60 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        appBar: AppBar(title: Text('美好人间')),
-        body: Container(
-          child: Column(
-            children: <Widget>[
-              TextField(
-                controller: typeController,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(10.0),
-                  labelText: '美女类型',
-                  helperText: '请输入你喜欢的类型',
-                ),
-                autofocus: false,
+          appBar: AppBar(title: Text('美好人间')),
+          body: SingleChildScrollView(
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    controller: typeController,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(10.0),
+                      labelText: '美女类型',
+                      helperText: '请输入你喜欢的类型',
+                    ),
+                    autofocus: false,
+                  ),
+                  RaisedButton(
+                    onPressed: _choiceAction,
+                    child: Text('选择完毕'),
+                  ),
+                  Text(
+                    showText,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 10,
+                  )
+                ],
               ),
-              RaisedButton(
-                onPressed: _choiceAction,
-                child: Text('选择完毕'),
-              ),
-              Text(
-                showText,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 10,
-              )
-            ],
-          ),
-        ),
-      ),
+            ),
+          )),
     );
   }
 
-  void _choiceAction(){
+  void _choiceAction() {
     print('开始选择你喜欢的类型.......');
-    if(typeController.text.toString() == ''){
+    if (typeController.text.toString() == '') {
       showDialog(
-        context: context,
-        builder: (context)=> AlertDialog(title: Text('内容不能为空'))
-      );
-    }else{
-      getHttp(typeController.text.toString()).then((val){
+          context: context,
+          builder: (context) => AlertDialog(title: Text('内容不能为空')));
+    } else {
+      getHttp(typeController.text.toString()).then((val) {
         setState(() {
           print(val);
-          showText=val['result'].toString();
+          showText = val['result'].toString();
         });
       });
     }
   }
-  Future getHttp(String TypeText) async{
-    try{
+
+  Future getHttp(String TypeText) async {
+    try {
       Response response;
-      var data = {'name':TypeText};
-      response = await Dio().get("https://api.apiopen.top/searchMusic",
-        queryParameters: data
-      );
+      var data = {'name': TypeText};
+      response = await Dio()
+          .get("https://api.apiopen.top/searchMusic", queryParameters: data);
       return response.data;
-    }catch(e){
+    } catch (e) {
       return print(e);
     }
   }
