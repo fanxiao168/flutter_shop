4b96d4da80fdedab28e08efc3c7ac1870053f1ca
fanxiao168 <fanxiao168@126.com>
2019-12-11 17:39:42 +0800

    Dio基础_伪造请求头获取数据

---
+++ flutter_shop/lib/config/httpHeaders.dart
@@ -0,0 +1,16 @@
+const httpHeaders = {
+  'Accept': 'application/json, text/plain, */*',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Accept-Language': 'zh-CN,zh;q=0.9',
+  'Connection': 'keep-alive',
+  'Content-Length': '0',
+  'Cookie':
+      'gksskpitn=45bcdd08-05f3-4eb0-8c63-d50a40b31041; _ga=GA1.2.651159308.1576054160; _gid=GA1.2.1886931482.1576054160; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1576054160; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1576054160; GRID=f79d0ef-41ac2b0-7d6b081-cb06850; MEIQIA_TRACK_ID=1UpWpHh2vHhHfdbAstr8v2BEPZI; MEIQIA_VISIT_ID=1UpWpDfJIOxTygb1YaFQoGdZIqL; SERVERID=3431a294a18c59fc8f5805662e2bd51e|1576054251|1576054155; _gat=1',
+  'Host': 'time.geekbang.org',
+  'Origin': 'https://time.geekbang.org',
+  'Referer': 'https://time.geekbang.org/',
+  'Sec-Fetch-Mode': 'cors',
+  'Sec-Fetch-Site': 'same-origin',
+  'User-Agent':
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+};
--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import '../config/httpHeaders.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,36 +8,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController typeController = TextEditingController();
-  String showText = '欢迎您来到美好人间高级会所';
+ 
+  String showText = '还没有请求数据';
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-          appBar: AppBar(title: Text('美好人间')),
+          appBar: AppBar(title: Text('请求远程数据')),
           body: SingleChildScrollView(
             child: Container(
               child: Column(
                 children: <Widget>[
-                  TextField(
-                    controller: typeController,
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.all(10.0),
-                      labelText: '歌名',
-                      helperText: '请输入歌名',
-                    ),
-                    autofocus: false,
-                  ),
                   RaisedButton(
-                    onPressed: _choiceAction,
-                    child: Text('选择完毕'),
+                    onPressed: _jike,
+                    child: Text('请求数据'),
                   ),
-                  Text(
-                    showText,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 10,
-                  )
+                  Text(showText)
                 ],
               ),
             ),
@@ -44,28 +32,22 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  void _choiceAction() {
-    print('开始选择歌名.......');
-    if (typeController.text.toString() == '') {
-      showDialog(
-          context: context,
-          builder: (context) => AlertDialog(title: Text('内容不能为空')));
-    } else {
-      postHttp(typeController.text.toString()).then((val) {
-        setState(() {
-          print(val);
-          showText = val['result'].toString();
-        });
+  void _jike() {
+    print('开始向极客时间请求数据.......');
+    getHttp().then((val){
+      setState(() {
+        showText=val.toString();
       });
-    }
+    });
   }
 
-  Future postHttp(String TypeText) async {
+  Future getHttp() async {
     try {
       Response response;
-      var data = {'name': TypeText};
-      response = await Dio()
-          .post("https://api.apiopen.top/searchMusic", queryParameters: data);
+      Dio dio = new Dio();
+      dio.options.headers = httpHeaders;
+      response = await dio.get("https://time.geekbang.org/serv/v2/explore/getBannerList");
+      print(response);
       return response.data;
     } catch (e) {
       return print(e);
