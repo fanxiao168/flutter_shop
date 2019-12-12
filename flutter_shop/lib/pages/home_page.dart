4d7a5a77afaff4af793f3b967acc3036ab1952c1
fanxiao168 <fanxiao168@126.com>
2019-12-12 16:34:11 +0800

    首页_屏幕适配方案

--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -51,8 +52,15 @@ class SwiperDiy extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    ScreenUtil.instance = ScreenUtil(width: 750,height: 1334)..init(context);
+    print('设备像素密度:${ScreenUtil.pixelRatio}');
+    print('设备的高:${ScreenUtil.screenHeight}');
+    print('设备的宽:${ScreenUtil.screenWidth}');
+    
     return Container(
-      height: 200,
+      height: ScreenUtil().setHeight(333),
+      width: ScreenUtil().setWidth(750),
       child: Swiper(
         itemBuilder: (BuildContext context, int index){
         return Image.network("${swiperDataList[index]['image']}",fit: BoxFit.fill,);
