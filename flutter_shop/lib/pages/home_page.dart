1046366227b3e9d4b5b64f864ab706ea9edd28f3
fanxiao168 <fanxiao168@126.com>
2019-12-13 11:57:39 +0800

    首页_url_launcher一键拨打店长电话

--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import '../service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -23,15 +24,21 @@ class _HomePageState extends State<HomePage> {
             future: getHomePageContent(),
             builder: (context,snapshot){
               if(snapshot.hasData){
+                //数据处理
                 var data = json.decode(snapshot.data.toString());
                 List<Map> swiper = (data['data']['slides'] as List).cast();
                 List<Map> navigatorList = (data['data']['category'] as List).cast();
                 String adPicture  = data['data']['advertesPicture']['PICTURE_ADDRESS'];
+                String leaderImage = data['data']['shopInfo']['leaderImage'];
+                String leaderPhone = data['data']['shopInfo']['leaderPhone'];
+
                 return Column(
                   children: <Widget>[
                     SwiperDiy(swiperDataList: swiper),
                     TopNavigator(navigatorList:navigatorList),
-                    AdBanner(adPicture:adPicture)
+                    AdBanner(adPicture:adPicture),
+                    LeaderPhone(leaderImage: leaderImage,leaderPhone: leaderPhone),
+
                   ],
                 );
               }else{
@@ -72,7 +79,7 @@ class SwiperDiy extends StatelessWidget {
   }
 }
 
-//首页导航组件
+//顶部导航
 class TopNavigator extends StatelessWidget {
 
   final List navigatorList;
@@ -111,7 +118,7 @@ class TopNavigator extends StatelessWidget {
   }
 }
 
-//首页广告条
+//广告区域
 class AdBanner extends StatelessWidget {
   
   final String adPicture;
@@ -122,5 +129,32 @@ class AdBanner extends StatelessWidget {
     return Container(
       child: Image.network(adPicture),
     );
+  }
+}
+
+//店长电话模块
+class LeaderPhone extends StatelessWidget {
+  final String leaderImage; //店长头像
+  final String leaderPhone; //店长电话
+
+  LeaderPhone({Key key,this.leaderImage,this.leaderPhone}):super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: InkWell(
+        onTap: _launchURL,
+        child: Image.network(leaderImage),
+      ),
+    );
+  }
+
+  void _launchURL() async{
+    String url = 'tel:' + leaderPhone;
+    if(await canLaunch(url)){
+      await launch(url);
+    }else{
+      throw 'url不能进行访问,异常';
+    }
   }
 }
