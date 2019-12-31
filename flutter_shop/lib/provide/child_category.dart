769087dbade13d5084faba831166a4cacdfb29cd
fanxiao168 <fanxiao168@126.com>
2019-12-31 11:52:33 +0800

    列表页_小类高亮交互效果制作

--- flutter_shop/lib/provide/child_category.dart
+++ flutter_shop/lib/provide/child_category.dart
@@ -4,9 +4,11 @@ import '../model/category.dart';
 
 class ChildCategory with ChangeNotifier{
   List<BxMallSubDto> childCategoryList = [];
+  int childIndex = 0;  //子类高亮索引
 
+//大类切换逻辑
   getChildCategory(List<BxMallSubDto> list){
-
+    childIndex = 0;
     BxMallSubDto all = BxMallSubDto();
     all.mallSubId = '00';
     all.mallCategoryId = '00';
@@ -15,6 +17,12 @@ class ChildCategory with ChangeNotifier{
     childCategoryList = [all];
     childCategoryList.addAll(list);
 
+    notifyListeners();
+  }
+
+  //改变子类索引
+  changeChildIndex(index){
+    childIndex = index;
     notifyListeners();
   }
 }
