7116f5359a64781a602d42e092f2e3d2e0a3d401
fanxiao168 <fanxiao168@126.com>
2020-01-03 17:15:43 +0800

    持久化_shared_preferences基础1

--- flutter_shop/lib/pages/cart_page.dart
+++ flutter_shop/lib/pages/cart_page.dart
@@ -1,50 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:provide/provide.dart';
-import '../provide/counter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class CartPage extends StatelessWidget {
+
+class CartPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Number(),
-            MyButton()
-          ],
-        ),
-      ),
-    );
-  }
+  _CartPageState createState() => _CartPageState();
 }
 
-class Number extends StatelessWidget {
+class _CartPageState extends State<CartPage> {
+
+  List<String> testList = [];
   @override
   Widget build(BuildContext context) {
+    _show();
     return Container(
-      margin: EdgeInsets.only(top: 200),
-      child: Provide<Counter>(
-        builder:(context,child,counter){
-          return Text(
-            '${counter.value}',
-            style:Theme.of(context).textTheme.display1
-            );
-        }
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 500.0,
+            child: ListView.builder(
+              itemCount: testList.length,
+              itemBuilder: (context,index){
+                return ListTile(
+                  title: Text(testList[index]),
+                );
+              },
+            ),
+          ),
+          RaisedButton(
+            onPressed: (){
+              _add();
+            },
+            child: Text('增加'),
+          ),
+
+          RaisedButton(
+            onPressed: (){
+              _clear();
+            },
+            child: Text('清空'),
+          ),
+
+        ],
       ),
     );
   }
+
+void _add() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String temp = "技术胖是最胖的";
+  testList.add(temp);
+  prefs.setStringList('testInfo', testList);
 }
 
-class MyButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: RaisedButton(
-        onPressed: (){
-          Provide.value<Counter>(context).increment();
-        },
-        child: Text('递增'),
-      ),
-    );
-  }
+void _show() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  setState(() {
+    if(prefs.getStringList('testInfo') != null){
+      testList = prefs.getStringList('testInfo');
+    }
+  });
+}
+
+void _clear() async{
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.remove('testInfo');
+  setState(() {
+    testList = [];
+  });
+}
+
 }
