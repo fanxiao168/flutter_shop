2c0d48bc540017e5d46e9bc5a7d00061e2c46c74
fanxiao168 <fanxiao168@126.com>
2020-01-09 17:29:13 +0800

    购物车_商品选中功能制作

--- flutter_shop/lib/provide/cart.dart
+++ flutter_shop/lib/provide/cart.dart
@@ -11,6 +11,8 @@ class CartProvide with ChangeNotifier {
   double allPrice = 0;  //总价格
   int allGoodsCount = 0; //商品总数量
 
+  bool isAllCheck = true; //是否全选
+
   
   save(goodsId,goodsName,count,price,images) async {
     //初始化SharedPreferences
@@ -70,10 +72,13 @@ class CartProvide with ChangeNotifier {
       List<Map> tempList = (json.decode(cartString.toString()) as List).cast();
       allPrice = 0;
       allGoodsCount = 0;
+      isAllCheck = true;
       tempList.forEach((item){
         if(item['isCheck']){
           allPrice+=item['count']*item['price'];
           allGoodsCount+=item['count'];
+        }else{
+          isAllCheck = false;
         }
         cartList.add(new CartInfoModel.fromJson(item));
       });
@@ -110,4 +115,43 @@ class CartProvide with ChangeNotifier {
     prefs.setString('cartInfo', cartString);
     await getCartInfo();
   }
+
+  changeCheckState(CartInfoModel cartItem) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    cartString = prefs.getString('cartInfo'); //得到持久化的字符串
+    List<Map> tempList = (json.decode(cartString.toString()) as List).cast();
+    int tempIndex = 0; //循环使用索引
+    int changeIndex = 0; //需要修改的索引
+
+    tempList.forEach((item){
+      if(item['goodsId'] == cartItem.goodsId){
+        //找到索引进行复制
+        changeIndex = tempIndex;
+      }
+      tempIndex++;
+    });
+
+    tempList[changeIndex] = cartItem.toJson(); // 把对象变成Map值
+    cartString = json.encode(tempList).toString(); //变成字符串
+    prefs.setString('cartInfo', cartString); //进行持久化
+    await getCartInfo(); //重新读取列表
+  }
+
+  //点击全选按钮操作
+  changeAllCheckBtnState(bool isCheck) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    cartString = prefs.getString('cartInfo');
+    List<Map> tempList = (json.decode(cartString.toString()) as List).cast();
+    List<Map> newList = []; //新建一个List,用于组成新的持久化数据
+    for(var item in tempList) {
+      var newItem = item; //复制新的变量,因为Dart不让循环时修改原值
+      newItem['isCheck'] = isCheck; // 改变选中的状态
+      newList.add(newItem);
+    }
+
+    cartString = json.encode(newList).toString();// 形成字符串
+    prefs.setString('cartInfo', cartString); //进行持久化
+    await getCartInfo();
+  }
+
 }
